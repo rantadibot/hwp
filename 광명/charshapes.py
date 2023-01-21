63555861e50d20fe1cb35238d6c6ae0a9da36007
rantadibot <hyangchos@gmail.com>
2023-01-21 16:36:31 +0900

    1.21 v2

--- 광명/charshapes.py
+++ 광명/charshapes.py
@@ -4,5 +4,7 @@ def char(color):
     hwp.Run(f"CharShapeTextColor{color.capitalize()}")
 def char2(effect):
     hwp.HAction.Run(f"CharShape{effect.capitalize()}")    
+def char3(effect):
+    hwp.HAction.Run(f"Break{effect.capitalize()}")    
 
 
--- 광명/tk2.1.py
+++ 광명/tk2.1.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from start2 import mod_hwp,hwp
-from charshapes import char,char2
+from charshapes import char,char2,char3
 
 def start():
     hwp.Run("MoveDocBegin")
@@ -15,7 +15,11 @@ def height2():
 def height3():
     hwp.HAction.Run("ParagraphShapeIncreaseLineSpacing")    
 def height4():
-    hwp.HAction.Run("ParagraphShapeDecreaseLineSpacing")    
+    hwp.Run("ParagraphShapeDecreaseLineSpacing")      
+def select():
+    hwp.Run("Select")      
+def select_all():
+    hwp.Run("SelectAll")      
 def hwp_exit():
     hwp.XHwpDocuments.Item(0).Close(isDirty=False)  # 탭 닫기
     hwp.Quit()
@@ -34,20 +38,26 @@ if __name__ == '__main__':
     button0.grid(column=0, row=0)
     button1 = tk.Button(text="문서 끝으로", command=end)
     button1.grid(column=1, row=0)
+    
     button1 = tk.Button(text="글씨 검은색", command=lambda:char('black'))
     button1.grid(column=0, row=1)
     button2 = tk.Button(text="글씨 빨간색", command=lambda:char('red'))
     button2.grid(column=1, row=1)
     button3 = tk.Button(text="글씨 청록색", command=lambda:char('bluish'))
     button3.grid(column=2, row=1)
+    button3 = tk.Button(text="글씨 파란색", command=lambda:char('blue'))
+    button3.grid(column=3, row=1)
+    
     button4 = tk.Button(text="글씨 진하게", command=lambda:char2('bold'))
     button4.grid(column=0, row=2)
     button5 = tk.Button(text="글씨 이탤릭", command=lambda:char2('italic'))
     button5.grid(column=1, row=2)
     button5 = tk.Button(text="글씨 밑줄", command=lambda:char2('underline'))
     button5.grid(column=2, row=2)
+    
     button6 = tk.Button(text="한글 종료", command=hwp_exit)
     button6.grid(column=0, row=3)
+    
     button6 = tk.Button(text="글자크기 +", command=height1)
     button6.grid(column=0, row=4)
     button6 = tk.Button(text="글자크기 -", command=height2)
@@ -56,4 +66,16 @@ if __name__ == '__main__':
     button6.grid(column=2, row=4)
     button6 = tk.Button(text="줄간격 -", command=height4)
     button6.grid(column=3, row=4)
+    
+    button7 = tk.Button(text="새 줄", command=lambda:char3('line'))
+    button7.grid(column=0, row=5)
+    button7 = tk.Button(text="새페이지", command=lambda:char3('page'))
+    button7.grid(column=1, row=5)
+    button7 = tk.Button(text="새문단", command=lambda:char3('para'))
+    button7.grid(column=2, row=5)
+    
+    button7 = tk.Button(text="단어선택", command=select)
+    button7.grid(column=0, row=6)
+    button7 = tk.Button(text="전체선택", command=select_all)
+    button7.grid(column=1, row=6)
     win.mainloop()
